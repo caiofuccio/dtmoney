9590dbabee0c229e8897983a4596b6d212b3d8ad
Caio Fuccio <caio.fuccio@gmail.com>
2021-03-17 00:46:34 -0300

    created the Header component and styled component

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -40,6 +40,15 @@ export const GlobalStyle = createGlobalStyle`
     cursor: pointer;
   }
 
+  body, input, textarea, button {
+    font-family: 'Poppins', sans-serif;
+    font-weight: 400;
+  }
+
+  h1, h2, h3, h4, h5, h6, strong {
+    font-weight: 600;
+  }
+
   [disabled] {
     opacity: 0.6;
     cursor: not-allowed;
