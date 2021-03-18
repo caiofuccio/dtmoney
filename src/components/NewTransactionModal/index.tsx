9149a16bb78641e23f47be867f8a7608335871d8
Caio Fuccio <caio.fuccio@gmail.com>
2021-03-17 23:34:21 -0300

    created the NewTransactionModal component and moved all the Modal content from App.tsx to it

---
+++ src/components/NewTransactionModal/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Modal from 'react-modal';
+
+interface NewTransactionModalProps {
+  isOpen: boolean;
+  onRequestClose: () => void;
+}
+
+export function NewTransactionModal({
+  isOpen,
+  onRequestClose,
+}: NewTransactionModalProps) {
+  return (
+    <Modal isOpen={isOpen} onRequestClose={onRequestClose}>
+      <h2>Cadastrar Transação</h2>
+    </Modal>
+  );
+}
