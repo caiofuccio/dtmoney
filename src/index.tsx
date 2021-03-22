26c3d87fda40a9beb04708728bd2a1ccc9f2d733
Caio Fuccio <caio.fuccio@gmail.com>
2021-03-21 21:40:51 -0300

    disabled server seeds

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
+import { createServer, Model } from 'miragejs';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createServer, Model } from 'miragejs';
 import App from './App';
 
 createServer({
@@ -8,28 +8,28 @@ createServer({
     transaction: Model,
   },
 
-  seeds(server) {
-    server.db.loadData({
-      transactions: [
-        {
-          id: 1,
-          title: 'Freelance de website',
-          type: 'deposit',
-          category: 'Dev',
-          amount: 6000,
-          createdAt: new Date('2021-02-12 09:00:00'),
-        },
-        {
-          id: 2,
-          title: 'Aluguel',
-          type: 'withdraw',
-          category: 'Casa',
-          amount: 1100,
-          createdAt: new Date('2021-02-14 11:00:00'),
-        },
-      ],
-    });
-  },
+  // seeds(server) {
+  //   server.db.loadData({
+  //     transactions: [
+  //       {
+  //         id: 1,
+  //         title: 'Freelance de website',
+  //         type: 'deposit',
+  //         category: 'Dev',
+  //         amount: 6000,
+  //         createdAt: new Date('2021-02-12 09:00:00'),
+  //       },
+  //       {
+  //         id: 2,
+  //         title: 'Aluguel',
+  //         type: 'withdraw',
+  //         category: 'Casa',
+  //         amount: 1100,
+  //         createdAt: new Date('2021-02-14 11:00:00'),
+  //       },
+  //     ],
+  //   });
+  // },
 
   routes() {
     this.namespace = 'api';
